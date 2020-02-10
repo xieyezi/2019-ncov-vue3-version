2a2e9b8c020243ce382e41c2a5415d73c0d5681e
xieyezi <1435398529@qq.com>
2020-02-10 22:58:44 +0800

    perf🐎: vue.config.js配置，优化打包体积

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,27 @@
 const path = require('path')
-function resolve (dir) {
-    return path.join(__dirname, dir)
+// 安装compression-webpack-plugin 压缩js为gzip
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
 }
 module.exports = {
-    lintOnSave: true,
-    chainWebpack: (config)=>{
-        config.resolve.alias
-            .set('@', resolve('src'))
-            .set('assets',resolve('src/assets'))
-            .set('components',resolve('src/components'))
-            .set('page',resolve('src/page'))
-            .set('utils',resolve('src/utils'))
-    }
+  lintOnSave: true,
+  productionSourceMap: false,  // 生产环境的 source map
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+      .set('page', resolve('src/page'))
+      .set('utils', resolve('src/utils'))
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$/,
+        threshold: 1024
+      })
+    ]
+  }
 }
